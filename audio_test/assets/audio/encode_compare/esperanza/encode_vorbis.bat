d0404212ede10f4574d5f1b2ce283d259df9c48c
daydreamer-json <atarakima1@icloud.com>
2025-09-06 18:16:25 +0900

    Change codec

--- audio_test/assets/audio/encode_compare/esperanza/encode_vorbis.bat
+++ audio_test/assets/audio/encode_compare/esperanza/encode_vorbis.bat
@@ -1,11 +1,5 @@
 @echo off&chcp 65001&cd /d %~dp0&cls
 
-oggenc -b 6 --converter 0 original.flac -o vorbis_6.ogg
-oggenc -b 8 --converter 0 original.flac -o vorbis_8.ogg
-oggenc -b 12 --converter 0 original.flac -o vorbis_12.ogg
-oggenc -b 16 --converter 0 original.flac -o vorbis_16.ogg
-oggenc -b 20 --converter 0 original.flac -o vorbis_20.ogg
-oggenc -b 24 --converter 0 original.flac -o vorbis_24.ogg
 oggenc -b 32 --converter 0 original.flac -o vorbis_32.ogg
 oggenc -b 40 --converter 0 original.flac -o vorbis_40.ogg
 oggenc -b 48 --converter 0 original.flac -o vorbis_48.ogg
@@ -15,13 +9,7 @@ oggenc -b 80 --converter 0 original.flac -o vorbis_80.ogg
 oggenc -b 96 --converter 0 original.flac -o vorbis_96.ogg
 oggenc -b 128 --converter 0 original.flac -o vorbis_128.ogg
 oggenc -b 192 --converter 0 original.flac -o vorbis_192.ogg
-
-ffmpeg -loglevel warning -i vorbis_6.ogg -vn -c:a copy vorbis_6.webm
-ffmpeg -loglevel warning -i vorbis_8.ogg -vn -c:a copy vorbis_8.webm
-ffmpeg -loglevel warning -i vorbis_12.ogg -vn -c:a copy vorbis_12.webm
-ffmpeg -loglevel warning -i vorbis_16.ogg -vn -c:a copy vorbis_16.webm
-ffmpeg -loglevel warning -i vorbis_20.ogg -vn -c:a copy vorbis_20.webm
-ffmpeg -loglevel warning -i vorbis_24.ogg -vn -c:a copy vorbis_24.webm
+pause
 ffmpeg -loglevel warning -i vorbis_32.ogg -vn -c:a copy vorbis_32.webm
 ffmpeg -loglevel warning -i vorbis_40.ogg -vn -c:a copy vorbis_40.webm
 ffmpeg -loglevel warning -i vorbis_48.ogg -vn -c:a copy vorbis_48.webm
